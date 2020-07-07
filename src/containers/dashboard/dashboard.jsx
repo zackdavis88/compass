2898ef56e1c1aac367288c95488fa3beec43de38
Zack Davis <zack.davis88@hotmail.com>
2020-07-07 12:06:01 -0700

    implement basic dashboard tabs without real content

--- src/containers/dashboard/dashboard.jsx
+++ src/containers/dashboard/dashboard.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {DashboardWrapper} from "./dashboard.styles";
-import { faTachometerAlt } from "@fortawesome/free-solid-svg-icons";
-import PageHeader from "../../components/page-header/page-header";
+import Tabs from "../../components/tabs/tabs";
 
 const Dashboard = (props) => {
   return (
     <DashboardWrapper>
-      <PageHeader dataTestId="dashboardPageHeader" text="My Dashboard" icon={faTachometerAlt} textCenter/>
+      {/* TODO: IMPLEMENT A GENERIC TAB COMPONENT
+            I want to have a dashboard with 2 tabs: My Projects and My Tasks.
+            - My Projects will contain all projects that you are a member of.
+            - My Tasks will contain all stories that are assigned to you.
+      */}
+      <Tabs dataTestId="dashboardTabs">
+        <Tabs.TabHeaders>
+          <Tabs.Header>My Projects</Tabs.Header>
+          <Tabs.Header>My Tasks</Tabs.Header>
+        </Tabs.TabHeaders>
+        <Tabs.TabPanels>
+          <Tabs.Panel>Hello, World!</Tabs.Panel>
+          <Tabs.Panel>This is some srs content.</Tabs.Panel>
+        </Tabs.TabPanels>
+      </Tabs>
     </DashboardWrapper>
   );
 };
--- src/containers/dashboard/dashboard.styles.js
+++ src/containers/dashboard/dashboard.styles.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
+import {TabsWrapper} from "../../components/tabs/tabs.styles";
 
 export const DashboardWrapper = styled.div`
+  height: 100%;
+  width: 100%;
 
+  & ${TabsWrapper} {
+    padding-top: 30px;
+    margin-left: 50px;
+    margin-right: 50px;
+  }
 `;
