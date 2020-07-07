89ff66232048528d96aa8ccecf7d58105d213254
Zack Davis <zack.davis88@hotmail.com>
2020-07-07 08:28:34 -0700

    add unstyled and untested Tabs component

---
+++ src/components/tabs/tabs.styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const TabsWrapper = styled.div`
+  position: relative;
+`;
+
+export const TabHeadersWrapper = styled.div`
+  border-bottom: 1px solid black;
+`;
+
+export const HeaderWrapper = styled.div`
+  display: inline-block;
+`;
+
+export const TabPanelsWrapper = styled.div``;
+
+export const PanelWrapper = styled.div``;
