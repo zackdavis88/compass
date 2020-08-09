9683c2128c7f3bb6039bf9e09ddb9a3dab741ca6
Zack Davis <zack.davis88@hotmail.com>
2020-08-09 12:04:36 -0700

    add new Dashboard redux actions and tests

--- src/store/reducers/dashboard/dashboard.spec.js
+++ src/store/reducers/dashboard/dashboard.spec.js
@@ -1,5 +1,5 @@
 import dashboardReducer from "./dashboard";
-import {getDashboard} from "../../actions/dashboard";
+import {getDashboardProjects, getDashboardStories} from "../../actions/dashboard";
 import {mockStore} from "../../../test-utils";
 import { waitFor } from "@testing-library/react";
 
@@ -81,13 +81,18 @@ describe("Dashboard Reducer / Actions", () => {
     });
   });
 
-  it("should dispatch a redux API call to get dashboard data", async () => {
-    store.dispatch(getDashboard());
+  it("should dispatch a redux API call to get dashboard projects data", async () => {
+    store.dispatch(getDashboardProjects());
     await waitFor(() => expect(store.getActions()).toHaveLength(2));
-    // First action should be the REQUEST
     expect(store.getActions()[0].type).toBe("DASHBOARD_REQUEST_START");
+    const expectedTypes = ["DASHBOARD_REQUEST_SUCCESS", "DASHBOARD_REQUEST_FAILURE"];
+    expect(expectedTypes.indexOf(store.getActions()[1].type)).toBeTruthy();
+  });
 
-    // Second action is expected to be SUCCESS or FAILURE
+  it("should dispatch a redux API call to get dashboard stories data", async () => {
+    store.dispatch(getDashboardStories());
+    await waitFor(() => expect(store.getActions()).toHaveLength(2));
+    expect(store.getActions()[0].type).toBe("DASHBOARD_REQUEST_START");
     const expectedTypes = ["DASHBOARD_REQUEST_SUCCESS", "DASHBOARD_REQUEST_FAILURE"];
     expect(expectedTypes.indexOf(store.getActions()[1].type)).toBeTruthy();
   });
