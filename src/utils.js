35e336fe744ad5f7ee2cc0cb4dda9dd8069a0e0c
Zack Davis <zack.davis88@hotmail.com>
2020-08-11 14:47:42 -0700

    keeping track of membership/stories page via querystring

--- src/utils.js
+++ src/utils.js
@@ -26,5 +26,47 @@ export const getPermissionLevel = (roles) => {
     return "Viewer";
 
   return "None";
-    
+};
+
+// Taken from stackoverflow: https://stackoverflow.com/questions/5999118/how-can-i-add-or-update-a-query-string-parameter
+export const generateUrlWithQuery = (key, value, url) => {
+  if (!url) url = window.location.href;
+  var re = new RegExp("([?&])" + key + "=.*?(&|#|$)(.*)", "gi"),
+      hash;
+
+  if (re.test(url)) {
+      if (typeof value !== 'undefined' && value !== null) {
+          return url.replace(re, '$1' + key + "=" + value + '$2$3');
+      } 
+      else {
+          hash = url.split('#');
+          url = hash[0].replace(re, '$1$3').replace(/(&|\?)$/, '');
+          if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
+              url += '#' + hash[1];
+          }
+          return url;
+      }
+  }
+  else {
+      if (typeof value !== 'undefined' && value !== null) {
+          var separator = url.indexOf('?') !== -1 ? '&' : '?';
+          hash = url.split('#');
+          url = hash[0] + separator + key + '=' + value;
+          if (typeof hash[1] !== 'undefined' && hash[1] !== null) {
+              url += '#' + hash[1];
+          }
+          return url;
+      }
+      else {
+          return url;
+      }
+  }
+}
+
+// Also ripped from stackoverflow: https://stackoverflow.com/questions/8648892/how-to-convert-url-parameters-to-a-javascript-object
+export const generateObjectFromSearch = (searchString) => {
+  if(!searchString)
+    return {};
+  const search = searchString.substring(1);
+  return JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g,'":"') + '"}', function(key, value) { return key===""?value:decodeURIComponent(value)})
 };
