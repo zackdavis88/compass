dfcc9d59c8719313047decafea191c05d26d73bc
Zack Davis <zack.davis88@hotmail.com>
2020-09-25 19:46:26 -0700

    update Main styles to account for upcoming footer

--- src/containers/main/main.styles.js
+++ src/containers/main/main.styles.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
-import { navbarHeight } from "../../common-styles/variables";
+import { navbarHeight, footerHeight } from "../../common-styles/variables";
 
 export const MainWrapper = styled.main`
   position: relative;
   left: 0;
   top: ${navbarHeight};
   width: 100%;
-  height: calc(100% - ${navbarHeight});
+  height: calc(100% - ${navbarHeight} - ${footerHeight});
+  overflow-y: auto;
 `;
